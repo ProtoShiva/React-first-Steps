2eec25e11909dd512cbbefa725cb3aa9b1ad1849
Himanshu Gusain <himanshugusain9559@gmail.com>
2025-01-17 10:49:17 +0530

    react easy way

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+// const heading = React.createElement(
+//   "h1",
+//   { className: "my-heading" },
+//   "Hello World from React"
+// )
+//console.log(heading) // a react element gets returned which is basically a JS object
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// console.log(root)
+//root.render(heading) // this render makes the react element into a h1 element
 
-const heading = React.createElement(
-  "h1",
-  { className: "my-heading" },
-  "Hello World from React"
-)
-console.log(heading) // a react element gets returned which is basically a JS object
-const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(root)
-root.render(heading) // this render makes the react element into a h1 element
+// const jsxHeading = <h1>Hello world from React Component</h1>
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(jsxHeading)
+
+//React Component
+
+//const myheading = () => <h1>Hello world from React Component</h1>
+
+//const myheading2 = () => <h1>Hello world from React Component</h1> //same as line 14
+//const jsxHeading2 = myheading()
+
+//how to put react element inside react component
+
+// const title = <h1>Javascript</h1>
+// console.log(title)
+
+// const Myheading = () => (
+//   <div>
+//     {title}
+//     <h1>Hello World ji</h1>
+//   </div>
+// )
+// const root = ReactDOM.createRoot(document.getElementById("root"))
+// root.render(<Myheading />)
